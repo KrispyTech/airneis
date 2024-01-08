302827bfa2df5088f13dabe40f576e992b30f24e
『BLANK』 <alixberthelot03@gmail.com>
2024-01-08 11:16:34 +0100

    chores: adding eslint/prettier/nativewind
    Signing

--- mobile/App.js
+++ mobile/App.js
@@ -1,20 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react"
+import { Text, View } from "react-native"
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-blue-600">Hello World</Text>
     </View>
-  );
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App
