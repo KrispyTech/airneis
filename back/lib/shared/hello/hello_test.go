84d57428eab855ea1c5634667075926c434335db
Cedric Gautier <gautiercedric07@gmail.com>
2024-01-16 18:54:06 +0100

    AIRNEIS-31: Add missing packages to have test files

--- back/lib/shared/hello/hello_test.go
+++ back/lib/shared/hello/hello_test.go
@@ -1,18 +1,25 @@
 package hello_test
 
 import (
-	"airneis/lib/shared/hello"
+	. "airneis/lib/shared/hello"
+	"testing"
 
 	. "github.com/onsi/ginkgo"
+	"github.com/onsi/ginkgo/reporters"
 	. "github.com/onsi/gomega"
 )
 
+func TestHello(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecsWithDefaultAndCustomReporters(t, "Hello", []Reporter{reporters.NewJUnitReporter("test_report-hello.xml")})
+}
+
 var _ = Describe("HelloTest", func() {
 	Context("When we want to print hello", func() {
-		word, number := hello.HelloWorld()
+		word, number := HelloWorld()
 		It("should return a string with hello world and 25 as an integer", func() {
 			Expect(word).To(Equal("HelloWorld"))
-			Expect(number).To(Equal(28))
+			Expect(number).To(Equal(25))
 		})
 	})
 })
