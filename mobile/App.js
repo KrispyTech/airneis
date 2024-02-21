5714d8c24821e5192168eb37437d22afe22c0c1e
『BLANK』 <alixberthelot03@gmail.com>
2024-02-21 09:53:32 +0100

    fix: moving mobileConfig stuff into tailwind.config and stop using hard values

--- mobile/App.js
+++ mobile/App.js
@@ -1,6 +1,6 @@
 import React from "react"
 import Page from "./src/components/Page"
 
-const App = () => <Page></Page>
+const App = () => <Page />
 
 export default App
--- mobile/src/components/Header.jsx
+++ mobile/src/components/Header.jsx
@@ -1,20 +1,20 @@
 import { Text, View } from "react-native"
 import { MagnifyingGlassIcon, Bars3Icon, ShoppingCartIcon } from "react-native-heroicons/outline"
 import ButtonWithSvg from "./ButtonWithSvg"
-import mobileConfig from "../../mobileConfig.js"
+import { color, icons } from "../../tailwind.config.cjs"
 
 const Header = () => (
-  <View className="bg-[#382E2B] py-1 flex items-center flex-row justify-between px-4">
-    <Text className="text-[#FFEDE6] text-4xl font-extrabold">ÀIRNEIS</Text>
+  <View className={`bg-[${color.brown}] py-1 flex items-center flex-row justify-between px-4`}>
+    <Text className={`text-[${color.lightPink}] text-4xl font-extrabold`}>ÀIRNEIS</Text>
     <View className="flex flex-row items-center">
       <ButtonWithSvg>
-        <MagnifyingGlassIcon {...mobileConfig.icons.lgIcon} />
+        <MagnifyingGlassIcon {...icons.lgIcon} />
       </ButtonWithSvg>
       <ButtonWithSvg>
-        <ShoppingCartIcon {...mobileConfig.icons.lgIcon} style={{ marginLeft: 8 }} />
+        <ShoppingCartIcon {...icons.lgIcon} style={{ marginLeft: 8 }} />
       </ButtonWithSvg>
       <ButtonWithSvg>
-        <Bars3Icon {...mobileConfig.icons.xlIcon} style={{ marginLeft: 8 }} />
+        <Bars3Icon {...icons.xlIcon} style={{ marginLeft: 8 }} />
       </ButtonWithSvg>
     </View>
   </View>
