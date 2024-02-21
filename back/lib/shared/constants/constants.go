a9c24b5e034200e2000c135e781bb9aa578de223
Cedric Gautier <gautiercedric07@gmail.com>
2024-02-21 19:23:57 +0100

    Update constants file and refactor log with consts

--- back/lib/shared/constants/constants.go
+++ back/lib/shared/constants/constants.go
@@ -1,39 +1,56 @@
 package constants
 
+import "fmt"
+
+// Words
 const (
-	AutoMigrate string = "automigrate"
-	Boot        string = "boot"
-	Booted      string = "booted"
-	Build       string = "build"
-	Format      string = "format"
-	Get         string = "get"
-	Initialize  string = "initialize"
-	Load        string = "load"
-	Loaded      string = "loaded"
-	Marshall    string = "marshall"
-	Migrate     string = "migrate"
-	Prepare     string = "prepare"
-	Read        string = "read"
-	Started     string = "started"
-	Unmarshall  string = "unmarshall"
+	AutoMigrate   string = "automigrate"
+	Boot          string = "boot"
+	Booted        string = "booted"
+	Build         string = "build"
+	Format        string = "format"
+	Get           string = "get"
+	has           string = "has " // with blankspace
+	Has           string = "has"
+	Initialize    string = "initialize"
+	Load          string = "load"
+	Loaded        string = "loaded"
+	Marshall      string = "marshall"
+	Migrate       string = "migrate"
+	Prepare       string = "prepare"
+	Read          string = "read"
+	StagingEnv    string = "staging"
+	ProductionEnv string = "production"
+	Started       string = "started"
+	Unmarshall    string = "unmarshall"
+)
+
+// Phrases
+const (
+	BackConfigBooting string = "Backend configuration booting"
+	BackAppStarted    string = "Backend application, fiber started"
+	HelloWorld        string = "Hello World!"
+	Listen3000        string = ":3000"
+	RoutesDefined     string = "Routes defined"
+	unableTo          string = "unable to " // with blankspace
 )
 
 var SelectUnable = map[string]string{
-	AutoMigrate: "unable to automigrate",
-	Boot:        "unable to boot",
-	Build:       "unable to build",
-	Format:      "unable to format",
-	Get:         "unable to get",
-	Initialize:  "unable to initialize",
-	Load:        "unable to load",
-	Marshall:    "unable to marshall",
-	Migrate:     "unable to migrate",
-	Prepare:     "unable to prepare",
-	Read:        "unable to read",
-	Unmarshall:  "unable to unmarshall",
+	AutoMigrate: fmt.Sprint(unableTo, AutoMigrate),
+	Boot:        fmt.Sprint(unableTo, Boot),
+	Build:       fmt.Sprint(unableTo, Build),
+	Format:      fmt.Sprint(unableTo, Format),
+	Get:         fmt.Sprint(unableTo, Get),
+	Initialize:  fmt.Sprint(unableTo, Initialize),
+	Load:        fmt.Sprint(unableTo, Load),
+	Marshall:    fmt.Sprint(unableTo, Marshall),
+	Migrate:     fmt.Sprint(unableTo, Migrate),
+	Prepare:     fmt.Sprint(unableTo, Prepare),
+	Read:        fmt.Sprint(unableTo, Read),
+	Unmarshall:  fmt.Sprint(unableTo, Unmarshall),
 }
 
 var SelectHas = map[string]string{
-	Started: "has started",
-	Loaded:  "has loaded",
+	Started: fmt.Sprint(has, Started),
+	Loaded:  fmt.Sprint(has, Loaded),
 }
--- back/main.go
+++ back/main.go
@@ -2,24 +2,25 @@ package main
 
 import (
 	"github.com/KrispyTech/airneis/config"
+	c "github.com/KrispyTech/airneis/lib/shared/constants"
 	"github.com/gofiber/fiber/v2"
 	log "github.com/sirupsen/logrus"
 )
 
 func main() {
-	log.Info("Backend configuration booting")
+	log.Info(c.BackAppStarted)
 	_, err := config.InitializeConfig()
 	if err != nil {
 		log.Fatal("Unable to load config - ", err.Error())
 	}
 
 	app := fiber.New()
-	log.Info("Backend application, fiber started")
+	log.Info(c.BackAppStarted)
 
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello World!")
+	app.Get(c.HomeRoute, func(ctx *fiber.Ctx) error {
+		return ctx.SendString(c.HelloWorld)
 	})
 
-	log.Info("Routes defined")
-	log.Fatal(app.Listen(":3000"))
+	log.Info(c.RoutesDefined)
+	log.Fatal(app.Listen(c.Listen3000))
 }
