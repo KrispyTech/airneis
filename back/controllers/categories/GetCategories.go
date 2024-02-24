ec92a56d679a265b495dbebe01e01c1484c61a9c
Barnabé PILLIAUDIN <barnabe.pilliaudin@gmail.com>
2024-02-24 20:19:34 +0100

    AIRNEIS-52: Get all categories endpoint

--- back/controllers/categories/GetCategories.go
+++ back/controllers/categories/GetCategories.go
@@ -1,7 +1,29 @@
 package controllers
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"encoding/json"
+	"github.com/KrispyTech/airneis/config"
+	"github.com/KrispyTech/airneis/lib/shared/constants"
+	model "github.com/KrispyTech/airneis/src/db/models"
+	"github.com/gofiber/fiber/v2"
+	"strconv"
+)
 
 func GetCategories(ctx *fiber.Ctx) error {
-	return ctx.SendString("getCategories")
+	page, err := strconv.Atoi(ctx.Query(constants.Page, constants.DefaultPageValue))
+	if err != nil {
+		ctx.Status(constants.BadRequestStatus)
+		return ctx.SendString(constants.BadRequestMessage)
+	}
+
+	var categories []model.Category
+	config.Database.Limit(constants.PaginationLimit).Offset((page - 1) * constants.PaginationLimit).Find(&categories)
+
+	jsonCategories, err := json.Marshal(categories)
+	if err != nil {
+		ctx.Status(constants.InternalServerErrorStatus)
+		return ctx.SendString(constants.InternalServerErrorMessage)
+	}
+
+	return ctx.Send(jsonCategories)
 }
