723fb0d108e89f2cd35962f8fa7cd20402c79b7d
Barnabé PILLIAUDIN <barnabe.pilliaudin@gmail.com>
2024-02-24 21:05:28 +0100

    AIRNEIS-52: Update category endpoint

--- back/controllers/categories/EditCategories.go
+++ back/controllers/categories/EditCategories.go
@@ -1,10 +1,36 @@
 package controllers
 
 import (
-	"fmt"
+	"encoding/json"
+	"github.com/KrispyTech/airneis/config"
+	"github.com/KrispyTech/airneis/lib/shared/constants"
+	model "github.com/KrispyTech/airneis/src/db/models"
 	"github.com/gofiber/fiber/v2"
+	"strconv"
 )
 
 func EditCategory(ctx *fiber.Ctx) error {
-	return ctx.SendString(fmt.Sprint("Delete Category %s", ctx.Params("categoryID")))
+	categoryID, err := strconv.Atoi(ctx.Params("categoryID"))
+	if err != nil {
+		ctx.Status(constants.BadRequestStatus)
+		return ctx.SendString(constants.BadRequestMessage)
+	}
+
+	var category model.Category
+	config.Database.First(&category, categoryID)
+
+	if errParsing := ctx.BodyParser(&category); errParsing != nil {
+		ctx.Status(constants.BadRequestStatus)
+		return ctx.SendString(constants.BadRequestMessage)
+	}
+
+	config.Database.Save(&category)
+
+	jsonCategory, err := json.Marshal(category)
+	if err != nil {
+		ctx.Status(constants.InternalServerErrorStatus)
+		return ctx.SendString(constants.InternalServerErrorMessage)
+	}
+
+	return ctx.Send(jsonCategory)
 }
