b59b7f49bd2c7b6720510d8f57a06358c227a2dc
Barnabé PILLIAUDIN <117679829+BarnabePILLIAUDIN@users.noreply.github.com>
2024-03-01 22:22:50 +0100

    AIRNEIS-59: Mobile navigation (#59)
    * AIRNEIS-59: Mobile navigation

--- mobile/src/components/Page.jsx
+++ mobile/src/components/Page.jsx
@@ -2,12 +2,12 @@ import { SafeAreaView, ScrollView, View } from "react-native"
 import Header from "./Header"
 
 const Page = props => {
-  const { children } = props
+  const { children, navigation } = props
 
   return (
     <View className="h-screen flex flex-col w-full">
       <SafeAreaView className="bg-secondary"></SafeAreaView>
-      <Header />
+      <Header navigation={navigation} />
       <ScrollView className="grow overflow-y-auto flex flex-col w-full bg-primary">{children}</ScrollView>
     </View>
   )
