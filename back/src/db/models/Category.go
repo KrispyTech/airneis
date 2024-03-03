2b1e26e5cd906d39d921640e3e593bd265880db2
Barnabé PILLIAUDIN <barnabe.pilliaudin@gmail.com>
2024-03-03 18:04:55 +0100

    AIRNEIS-59: made ci work

--- back/src/db/models/Category.go
+++ back/src/db/models/Category.go
@@ -5,6 +5,7 @@ import (
 )
 
 type Category struct {
+	gorm.Model
 	Name           string    `json:"name"`
 	ThumbnailUrl   string    `json:"thumbnailUrl"`
 	Slug           string    `json:"slug" gorm:"unique"`
--- mobile/src/screens/HomeScreen.jsx
+++ mobile/src/screens/HomeScreen.jsx
@@ -1,43 +1,42 @@
-import {Button, FlatList, ScrollView, Text} from "react-native"
+import { Button, FlatList, ScrollView, Text } from "react-native"
 import Page from "../components/Page"
 import screens from "../utils/constants/screens"
-import {getCategories} from "../utils/categoriesServices"
-import {useEffect, useState} from "react"
+import { getCategories } from "../utils/categoriesServices"
+import { useEffect, useState } from "react"
 import ErrorScreen from "./ErrorScreen"
 import errors from "../utils/constants/errors"
 
 const HomeScreen = props => {
-    const {navigation} = props
-    const [categories, setCategories] = useState([])
-    const [hadError, setHadError] = useState(false)
-    const goToCategory = id => () => navigation.navigate(screens.categoryPage, {id})
+  const { navigation } = props
+  const [categories, setCategories] = useState([])
+  const [hadError, setHadError] = useState(false)
+  const goToCategory = id => () => navigation.navigate(screens.categoryPage, { id })
 
-    useEffect(() => {
-        ;(async () => {
-            try {
-                const categoriesResult = await getCategories()
-                setCategories(categoriesResult.data)
-            } catch (e) {
-                setHadError(true)
-            }
-        })()
-    }, [])
+  useEffect(() => {
+    ;(async () => {
+      try {
+        const categoriesResult = await getCategories()
+        setCategories(categoriesResult.data)
+      } catch (e) {
+        setHadError(true)
+      }
+    })()
+  }, [])
 
-    if (hadError) {
-        return <ErrorScreen navigation={navigation} text={errors.homePage.unableToLoadCategories}/>
-    }
+  if (hadError) {
+    return <ErrorScreen navigation={navigation} text={errors.homePage.unableToLoadCategories} />
+  }
 
-    return (
-        <Page navigation={navigation}>
-            <ScrollView>
-                <Text>Home page</Text>
-                {categories.map(({name, ID: id}) => (
-                    <Button title={name} onPress={goToCategory(id)} key={id}/>
-                ))}
-            </ScrollView>
-        </Page>
-    )
-
+  return (
+    <Page navigation={navigation}>
+      <ScrollView>
+        <Text>Home page</Text>
+        {categories.map(({ name, ID: id }) => (
+          <Button title={name} onPress={goToCategory(id)} key={id} />
+        ))}
+      </ScrollView>
+    </Page>
+  )
 }
 
 export default HomeScreen
