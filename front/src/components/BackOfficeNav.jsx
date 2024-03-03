ea6db1cdb5b8dbe0b662080bcf6a78d0197b9b47
『BLANK』 <alixberthelot03@gmail.com>
2024-03-03 20:29:36 +0100

    chores: backoffice + category page

---
+++ front/src/components/BackOfficeNav.jsx
@@ -0,0 +1,40 @@
+import BackOfficeNavPM from "@/components/BackOfficeNavPM"
+import { QuestionMarkCircleIcon, UserGroupIcon } from "@heroicons/react/24/outline"
+import clsx from "clsx"
+import Link from "next/link"
+import { useRouter } from "next/router"
+
+const BackOfficeNav = ({ isOpen, handleOpen }) => {
+  const router = useRouter()
+  const [page] = router.pathname.split("/").slice(-1)
+
+  return (
+    <nav
+      className={`h-full w-fit md:left-0 md:w-2/5 lg:w-1/3 md:static bg-tertiary border-r-2 z-20 ${
+        isOpen ? "left-0 ease-in-out duration-300 absolute" : "-left-48  ease-in-out duration-300 absolute"
+      }`}
+    >
+      <BackOfficeNavPM page={page} />
+      <ul className="flex flex-col px-3 gap-6 pb-6 ">
+        <li className="md:text-xl border-b leading-extra-loose md:leading-extra-loose">User Management</li>
+        <li className={clsx("py-1.5", page === "users" ? "bg-black/30 rounded-sm" : "")}>
+          <Link href="/back-office/users" className="text-lg md:text-2xl flex items-center font-bold gap-4">
+            <UserGroupIcon className="h-8 w-8" strokeWidth="2.25" />
+            Users
+          </Link>
+        </li>
+        <li className={clsx("py-1.5", page === "contacts" ? "bg-black/30 rounded-sm" : "")}>
+          <Link href="/back-office/contacts" className="text-lg md:text-2xl flex items-center font-bold gap-4">
+            <QuestionMarkCircleIcon className="h-8 w-8" strokeWidth="2.25" />
+            Contacts
+          </Link>
+        </li>
+        <button className="text-xl md:hidden mt-4 bg-black/10 border rounded-sm" onClick={handleOpen}>
+          Close
+        </button>
+      </ul>
+    </nav>
+  )
+}
+
+export default BackOfficeNav
---
+++ front/src/pages/back-office/categories.jsx
@@ -0,0 +1,88 @@
+import BackOfficeModal from "@/components/BackOfficeModal"
+import BackOfficeNav from "@/components/BackOfficeNav"
+import BackOfficeTableHead from "@/components/BackOfficeTableHead"
+import CategoryListItem from "@/components/CategoryListItem"
+import Page from "@/components/Page"
+import { Bars3Icon, PlusCircleIcon } from "@heroicons/react/24/outline"
+import Link from "next/link"
+import { useState } from "react"
+
+const categories = [
+  { id: 1, name: "Salon", image: "https://picsum.photos/90/70" },
+  { id: 2, name: "Chambre", image: "https://picsum.photos/90/70" },
+  { id: 3, name: "Salle de bain", image: "https://picsum.photos/90/70" },
+  { id: 4, name: "Cuisine", image: "https://picsum.photos/90/70" },
+  { id: 5, name: "Bureau", image: "https://picsum.photos/90/70" },
+  { id: 6, name: "Buanderie", image: "https://picsum.photos/90/70" },
+  { id: 7, name: "Jardin", image: "https://picsum.photos/90/70" },
+  { id: 8, name: "Garage", image: "https://picsum.photos/90/70" }
+]
+
+const CategoriesBoard = () => {
+  const [isOpen, setIsOpen] = useState(false)
+  const handleOpen = () => setIsOpen(!isOpen)
+  const [selected, setSelected] = useState({})
+  const [isModalOpen, setIsModalOpen] = useState(false)
+  const [itemId, setItemId] = useState(null)
+  const handleOpenModal = event => {
+    const id = event.currentTarget.getAttribute("data-id")
+    setItemId(id)
+    setIsModalOpen(!isModalOpen)
+  }
+
+  const handleChange = event => {
+    // More things to do when filter will be done.
+    const id = event.currentTarget.getAttribute("data-id")
+
+    if (id === "all") {
+      setSelected({ ...selected, ...categories.reduce((xs, x) => ({ ...xs, [x.id]: true }), {}) })
+
+      return
+    }
+
+    setSelected({ ...selected, [id]: !selected[id] })
+  }
+
+  const handleInput = () => {
+    // To do
+  }
+
+  return (
+    <Page page="backoffice">
+      <div className="flex h-full">
+        <BackOfficeNav isOpen={isOpen} handleOpen={handleOpen} />
+        <section className="flex flex-col w-full px-4 pt-3 overflow-y-scroll">
+          {isModalOpen ? <BackOfficeModal handleOpenModal={handleOpenModal} id={itemId} /> : ""}
+          <div className="flex justify-between">
+            <h1 className="font-bold text-2xl hidden md:block">Categories</h1>
+            <button onClick={handleOpen} className="md:hidden font-bold text-2xl flex items-center gap-2">
+              <Bars3Icon className="h-8 w-8" strokeWidth="2" />
+              Categories
+            </button>
+            <input placeholder="Search..." className="rounded-md drop-shadow-lg pl-2 w-40 md:w-72" onChange={handleInput} />
+          </div>
+          <Link href="/backoffice/add-product" className="mt-4 py-0.5 px-1.5 bg-gray w-fit rounded-full border flex gap-1 items-center">
+            <PlusCircleIcon className="w-6 h-6" />
+            Add
+          </Link>
+          <table className="mt-4">
+            <BackOfficeTableHead handleChange={handleChange} page="categories" />
+            <tbody>
+              {categories.map(category => (
+                <CategoryListItem
+                  key={category.id}
+                  category={category}
+                  selected={selected[category.id]}
+                  handleOpenModal={handleOpenModal}
+                  handleChange={handleChange}
+                />
+              ))}
+            </tbody>
+          </table>
+        </section>
+      </div>
+    </Page>
+  )
+}
+
+export default CategoriesBoard
