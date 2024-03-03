ddf2eebf97ce536d2b8b1c3fcbd22c83a84bc00d
Barnabé PILLIAUDIN <barnabe.pilliaudin@gmail.com>
2024-03-03 16:53:45 +0100

    AIRNEIS-59: Added description field to category

--- back/src/db/models/Category.go
+++ back/src/db/models/Category.go
@@ -1,12 +1,15 @@
 package model
 
-import "gorm.io/gorm"
+import (
+	"gorm.io/gorm"
+)
 
 type Category struct {
 	gorm.Model
 	Name           string    `json:"name"`
 	ThumbnailUrl   string    `json:"thumbnailUrl"`
 	Slug           string    `json:"slug" gorm:"unique"`
+	Description    string    `json:"description"`
 	Products       []Product `json:"products"`
 	OrderOfDisplay *uint     `json:"orderOfDisplay" gorm:"unique"`
 }
