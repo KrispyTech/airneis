7ef4cd782355978651a5070bd7c8a86ed9c9499c
『BLANK』 <alixberthelot03@gmail.com>
2024-03-05 17:00:01 +0100

    fix: making constants and routes file to match Cédric ask

--- front/src/components/Header.jsx
+++ front/src/components/Header.jsx
@@ -1,13 +1,15 @@
 import { MagnifyingGlassIcon, ShoppingCartIcon, Bars3Icon } from "@heroicons/react/24/outline"
 import webConfig from "@@/webConfig"
 import Link from "next/link"
+import { backoffice } from "@/constants"
+import routes from "@/routes"
 
 const Header = ({ page }) => (
   <header className="bg-secondary text-primary px-4 flex justify-between items-center sticky top-0 z-30">
-    <Link href="/" className="text-xl md:text-4xl py-3 font-extrabold">
+    <Link href={routes.home} className="text-xl md:text-4xl py-3 font-extrabold">
       ÀIRNEIS
     </Link>
-    {page !== "backoffice" ? (
+    {page !== backoffice ? (
       <nav>
         <ul className="flex flex-row items-center gap-5">
           <li>
