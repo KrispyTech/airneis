897435b85cca61448b1919a7f35af4aadf3ee6d1
Cedric Gautier <gautiercedric07@gmail.com>
2024-03-14 15:54:30 +0100

    Rework HTTP Client

--- front/src/utils/config.js
+++ front/src/utils/config.js
@@ -1,10 +1,6 @@
 const config = {
   api: {
-<<<<<<< HEAD
     url: process.env.API_URL
-=======
-    url: process.env.api_url
->>>>>>> 2c1182c (front-api: Created the functions for api call)
   }
 }
 
