7aa2d57cabe510ac532cb1c18b16382182b5d38f
Barnabé PILLIAUDIN <barnabe.pilliaudin@gmail.com>
2024-03-22 15:43:31 +0100

    AIRNEIS-106: Frontend translations + doc

--- front/src/components/Header.jsx
+++ front/src/components/Header.jsx
@@ -1,24 +1,44 @@
 import { MagnifyingGlassIcon, ShoppingCartIcon, Bars3Icon } from "@heroicons/react/24/outline"
 import webConfig from "@@/webConfig"
+import { useTranslation } from "next-i18next"
+import { useRouter } from "next/router"
 
-const Header = () => (
-  <header className="bg-secondary text-primary px-4 flex justify-between items-center">
-    <h1 className="text-4xl py-6">ÀIRNEIS</h1>
-    <nav>
-      <ul className="flex flex-row items-center gap-5">
-        <li>
-          <MagnifyingGlassIcon {...webConfig.icons.lgIcon} />
-        </li>
-        <li>
-          <ShoppingCartIcon {...webConfig.icons.lgIcon} />
-        </li>
-        <li>
-          {/* As this icon is smaller that the other we need it to have a bigger size */}
-          <Bars3Icon {...webConfig.icons.xlIcon} />
-        </li>
-      </ul>
-    </nav>
-  </header>
-)
+const Header = () => {
+  const { t } = useTranslation("common")
+  const { locales, push } = useRouter()
+  const changeLocale = locale => {
+    push("", "", { locale })
+  }
+
+  return (
+    <header className="bg-secondary text-primary px-4 flex justify-between items-center">
+      <h1 className="text-4xl py-6">ÀIRNEIS</h1>
+      <nav>
+        <ul className="flex flex-row items-center gap-5">
+          <li>
+            <select className="bg-secondary " onInput={({ target: { value } }) => changeLocale(value)}>
+              <option value="">{t("header.selectALanguage")}</option>
+              {locales.map(locale => (
+                <option key={locale} value={locale}>
+                  {t(`header.locales.${locale}`)}
+                </option>
+              ))}
+            </select>
+          </li>
+          <li>
+            <MagnifyingGlassIcon {...webConfig.icons.lgIcon} />
+          </li>
+          <li>
+            <ShoppingCartIcon {...webConfig.icons.lgIcon} />
+          </li>
+          <li>
+            {/* As this icon is smaller that the other we need it to have a bigger size */}
+            <Bars3Icon {...webConfig.icons.xlIcon} />
+          </li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
 
 export default Header
--- front/src/pages/index.js
+++ front/src/pages/index.js
@@ -1,9 +1,26 @@
 import Page from "@/components/Page"
+import { useTranslation } from "next-i18next"
+import { serverSideTranslations } from "next-i18next/serverSideTranslations"
+import Link from "next/link"
+import { useRouter } from "next/router"
 
-export default function Home() {
+export const getServerSideProps = async ({ locale }) => ({
+  props: {
+    ...(await serverSideTranslations(locale, ["common"]))
+  }
+})
+const Home = () => {
+  const { locale } = useRouter()
+  const { t } = useTranslation("common")
+
   return (
     <Page className="h-screen text-secondary">
-      <h2 className="text-2xl font-semibold py-5 px-3"> WIP come back later!</h2>
+      <h2 className="text-2xl font-semibold py-5 px-3"> {t("homePage.wip")}</h2>
+      <Link href="/" locale={locale}>
+        {t("homePage.linkExample")}
+      </Link>
     </Page>
   )
 }
+
+export default Home
