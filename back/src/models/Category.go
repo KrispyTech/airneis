3a8f92b1cd780e607e6d4629e74de6a098713b4d
Barnabé PILLIAUDIN <barnabe.pilliaudin@gmail.com>
2024-03-22 11:22:28 +0100

    AIRNEIS-59: Correcrted Url to URL

--- back/src/models/Category.go
+++ back/src/models/Category.go
@@ -5,7 +5,7 @@ import "gorm.io/gorm"
 type Category struct {
 	gorm.Model
 	Name           string    `json:"name"`
-	ThumbnailURL   string    `json:"thumbnailUrl"`
+	ThumbnailURL   string    `json:"thumbnailURL"`
 	Slug           string    `json:"slug"           gorm:"unique"`
 	Description    string    `json:"description"`
 	Products       []Product `json:"products"`
