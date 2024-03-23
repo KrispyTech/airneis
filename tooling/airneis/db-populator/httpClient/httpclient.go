6d52841aaa1e9684cc5d2032a38430f03d448e28
Ced <75094434+cedricgautier@users.noreply.github.com>
2024-03-23 18:10:36 +0100

    AIRNEIS-50 : Populate DB with categories  (#76)

---
+++ tooling/airneis/db-populator/httpClient/httpclient.go
@@ -0,0 +1,92 @@
+package httpclient
+
+import (
+	"bytes"
+	"encoding/json"
+	"io"
+	"net/http"
+
+	"github.com/pkg/errors"
+)
+
+const methodGet string = "GET"
+const methodPost string = "POST"
+
+type HttpApi interface {
+	Get(url string, headers map[string]string) ([]byte, int, error)
+	Post(url string, headers map[string]string, jsonBody []byte) ([]byte, int, error)
+	PrepareBody(reqBody interface{}) (body []byte, err error)
+}
+
+type HttpClient struct {
+	api *http.Client
+}
+
+type Request struct {
+	Body       []byte
+	Headers    map[string]string
+	Method     string
+	StatusCode int
+	Url        string
+}
+
+func InitializeHttpApi() HttpApi {
+	return &HttpClient{api: &http.Client{}}
+}
+
+func (c *HttpClient) PrepareBody(reqBody interface{}) (body []byte, err error) {
+	body, err = json.Marshal(reqBody)
+	if err != nil {
+		return nil, errors.Errorf("PrepareBody, Unable to marshall body:  %s", err)
+	}
+
+	return
+}
+
+func (c *HttpClient) Get(url string, headers map[string]string) (body []byte, statusCode int, err error) {
+	return c.makeRequest(Request{Url: url, Method: methodGet, Headers: headers})
+}
+
+func (c *HttpClient) Post(url string, headers map[string]string, jsonBody []byte) (body []byte, statusCode int, err error) {
+	return c.makeRequest(Request{Url: url, Method: methodPost, Headers: headers, Body: jsonBody})
+}
+
+func addHeaders(req *http.Request, headers map[string]string) *http.Request {
+	for k, v := range headers {
+		req.Header.Add(k, v)
+	}
+
+	return req
+}
+
+func (c *HttpClient) makeRequest(r Request) (body []byte, statusCode int, err error) {
+	req, err := http.NewRequest(r.Method, r.Url, bytes.NewReader(r.Body))
+	if err != nil {
+		return nil, 0, errors.Errorf("makeRequest,unable to make new request:  %s", err)
+	}
+
+	if r.Headers != nil {
+		req = addHeaders(req, r.Headers)
+	}
+
+	resp, err := c.api.Do(req)
+	if err != nil {
+		return nil, 0, err
+	}
+
+	return showResponse(resp)
+}
+
+func showResponse(resp *http.Response) ([]byte, int, error) {
+	if resp.StatusCode != http.StatusOK {
+		return nil, 0, errors.Errorf("Error %v: %s", resp.StatusCode, resp.Request.URL)
+	}
+
+	defer resp.Body.Close()
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, 0, errors.Errorf("showResponse,unable to ReadAll body:  %s", err)
+	}
+
+	return body, resp.StatusCode, nil
+}
