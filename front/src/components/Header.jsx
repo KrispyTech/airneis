afed49b8f0e2a1f2244918300b4cd1d64bafa06d
Barnabé PILLIAUDIN <117679829+BarnabePILLIAUDIN@users.noreply.github.com>
2024-03-24 20:39:47 +0100

    Revert "AIRNEIS-106: Frontend translations + doc (#81)"
    This reverts commit f2ac39a2b61f1cb8bd069b3d122cbe1859122e3c.

--- front/src/components/Header.jsx
+++ front/src/components/Header.jsx
@@ -1,30 +1,17 @@
 import { MagnifyingGlassIcon, ShoppingCartIcon, Bars3Icon } from "@heroicons/react/24/outline"
 import webConfig from "@@/webConfig"
-import { useTranslation } from "next-i18next"
-import { useRouter } from "next/router"
+import Link from "next/link"
+import { backoffice } from "@/constants"
+import routes from "@/routes"
 
-const Header = () => {
-  const { t } = useTranslation("common")
-  const { locales, push } = useRouter()
-  const changeLocale = locale => {
-    push("", "", { locale })
-  }
-
-  return (
-    <header className="bg-secondary text-primary px-4 flex justify-between items-center">
-      <h1 className="text-4xl py-6">ÀIRNEIS</h1>
+const Header = ({ page }) => (
+  <header className="bg-secondary text-primary px-4 flex justify-between items-center sticky top-0 z-30">
+    <Link href={routes.home} className="text-xl md:text-4xl py-3 font-extrabold">
+      ÀIRNEIS
+    </Link>
+    {page !== backoffice ? (
       <nav>
         <ul className="flex flex-row items-center gap-5">
-          <li>
-            <select className="bg-secondary " onInput={({ target: { value } }) => changeLocale(value)}>
-              <option value="">{t("header.selectLanguage")}</option>
-              {locales.map(locale => (
-                <option key={locale} value={locale}>
-                  {t(`header.locales.${locale}`)}
-                </option>
-              ))}
-            </select>
-          </li>
           <li>
             <MagnifyingGlassIcon {...webConfig.icons.lgIcon} />
           </li>
--- front/src/pages/index.js
+++ front/src/pages/index.js
@@ -1,26 +1,9 @@
 import Page from "@/components/Page"
-import { useTranslation } from "next-i18next"
-import { serverSideTranslations } from "next-i18next/serverSideTranslations"
-import Link from "next/link"
-import { useRouter } from "next/router"
 
-export const getServerSideProps = async ({ locale }) => ({
-  props: {
-    ...(await serverSideTranslations(locale, ["common"]))
-  }
-})
-const Home = () => {
-  const { locale } = useRouter()
-  const { t } = useTranslation("common")
-
+export default function Home() {
   return (
     <Page className="h-screen text-secondary">
-      <h2 className="text-2xl font-semibold py-5 px-3"> {t("homePage.wip")}</h2>
-      <Link href="/" locale={locale}>
-        {t("homePage.linkExample")}
-      </Link>
+      <h2 className="text-2xl font-semibold py-5 px-3"> WIP come back later!</h2>
     </Page>
   )
 }
-
-export default Home
