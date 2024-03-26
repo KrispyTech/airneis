f90e6ef482b36939d3a4c485b15980d141f05b4e
Magali-D <magali.paoli@tutanota.com>
2024-03-26 18:34:02 +0100

    Added CRUD + routes

---
+++ back/pkg/routes/products.go
@@ -0,0 +1,19 @@
+package routes
+
+import (
+	controllers "github.com/KrispyTech/airneis/src/controllers/products"
+	"github.com/gofiber/fiber/v2"
+)
+
+const (
+	productsRoute       = "products"
+	productsRouteWithID = "products/:categoryID"
+)
+
+func setProductsRoutes(app fiber.Router) {
+	app.Get(productsRoute, controllers.GetProducts)
+	app.Get(productsRouteWithID, controllers.GetProductByID)
+	app.Post(productsRoute, controllers.CreateProduct)
+	app.Patch(productsRouteWithID, controllers.EditProduct)
+	app.Delete(productsRouteWithID, controllers.DeleteProduct)
+}
